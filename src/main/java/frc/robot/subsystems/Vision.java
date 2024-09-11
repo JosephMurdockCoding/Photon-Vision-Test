da2e4db24aa9570c853a848d5f3ec39dcce2dd32
Joseph Murdock <JoeMurdockVirginia@gmail.com>
2024-09-11 19:54:37 -0400

    made things worse

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -7,26 +7,38 @@ package frc.robot.subsystems;
 import org.photonvision.PhotonCamera;
 
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
 
 public class Vision extends SubsystemBase {
   /** Creates a new Vision. */
   public Vision() {
-    final double CAMERA_HEIGHT_METERS = Units.inchesToMeters(24);
-    final double TARGET_HEIGHT_METERS = Units.feetToMeters(5);
-    final double CAMERA_PITCH_RADIANS = Units.degreesToRadians(0);
 
     PhotonCamera camera = new PhotonCamera("103Cybersonics");
 
-     double latencyMillis = 0;
-     boolean hasTarget = false;
-    double targetPitch = 0;
-     double targetYaw = 0;
-     double targetArea = 0;
-     double targetSkew = 0;
-     double targetPose = 0;
-     double targetPixelsX = 0;
-     double targetPixelsY = 0;
+     NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight-fv");
+
+     NetworkTableEntry rawBytes = table.getEntry("rawBytes");
+     NetworkTableEntry latencyMillis = table.getEntry("latencyMillis");
+     NetworkTableEntry targetPitch = table.getEntry("targetPitch");
+
+     NetworkTableEntry targetYaw = table.getEntry("targetYaw");
+     NetworkTableEntry targetArea = table.getEntry("targetArea");
+     NetworkTableEntry targetSkew = table.getEntry("targetSkew");
+
+     NetworkTableEntry targetPose = table.getEntry("targetPose");
+     NetworkTableEntry tltargetPixelsXEntry = table.getEntry("targetPixelsX");
+     NetworkTableEntry targetPixelsY = table.getEntry("targetPixelsY");
+
+     NetworkTableEntry targetspaceEntry = table.getEntry("botpose_targetspace");
+     double[] targetspaceCache = new double[6]; // array that will hold all the positions
+
+     NetworkTableEntry botpose_wpiblueEntry = table.getEntry("botpose_wpiblue");
+     double[] botpose_wpiblueCache = new double[7];
   }
 
   @Override
